e2935d85fdb522092c95bab2324292d189eadd58
Align807 <66521673+Align807@users.noreply.github.com>
2021-11-01 14:57:04 +0530

    corrected

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,49 +1,11 @@
-import React, { useState } from 'react';
-//import HeaderSection from "../components/Header/header";
-//import Footer from "../components/Footer/footer";
-import SearchIcon from 'mdi-react/SearchIcon';
-
-
-
-
-
-//import tableData from '../../../Data/tableData.json';
-import {Container} from 'semantic-ui-react';
-
-import { Icon, Label, Menu, Table, Modal, Header, Button, Checkbox, Grid  } from 'semantic-ui-react';
-//import _ from 'lodash';
-
-
-
+import React from 'react'
 
 export default function Dashboard() {
-  
- 
-   
-  return(
-
-<>
-    
-      
-    
-     
-    
-           
-        <h1 style={{"color": "black"}}>Farms </h1><span>&nbsp;&nbsp;</span>
-      
-      
-   {/*--------------------------------------------Add edit delete farm popup----------------------*/}
-     
-
-        <div className="spacer"></div>
-    
-  
-</>
-
-
-      );
+    return (
+        <div>
+            <h1>
+                Dashboard
+            </h1>
+        </div>
+    )
 }
-
-
-    
- 
